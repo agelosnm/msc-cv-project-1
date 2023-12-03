7f9e3d3aa62533b8d663f6b33e99113732128e38
Angelos Naoum <angelos@scio.systems>
2023-12-03 20:33:52 +0200

    modifying noise function for exersice 5

--- exercise_5.py
+++ exercise_5.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image, ImageFilter
+from PIL import Image
 
-def noise_image(image, mean=0, stg=0.01):
-    img_array = np.array(image)
-    noise = np.random.normal(mean, stg, img_array.shape)
-    noisy_image = img_array + noise
-    noisy_image = np.clip(noisy_image, 0, 255)  # Clip values to the valid range [0, 255]
-    noisy_image = noisy_image.astype(np.uint8)
-    return Image.fromarray(noisy_image)
+def noise_image(image, mean=0, std=0.01):
+    image = np.array(image)
+    shape = image.shape
+    noise = np.random.normal(mean, std, shape)
+    noisy_image = np.clip(image + noise, 0, 255).astype(np.uint8)
+    
+    return noisy_image
 
 def butterworth_low_pass_filter(image_fft, order, cutoff_freq):
     M,N = image_fft.shape
@@ -19,19 +19,19 @@ def butterworth_low_pass_filter(image_fft, order, cutoff_freq):
     for u in range(M):
         for v in range(N):
             D = np.sqrt((u-M/2)**2 + (v-N/2)**2)
-            H[u,v] = 1 / (1 + (D/D0)**n)
+            H[u,v] = 1 / (1 + (D/D0)**(n))
     
     filtered_image_fft = image_fft * H
     filtered_image = np.abs(np.fft.ifft2(np.fft.ifftshift(filtered_image_fft)))
+    
     return filtered_image
 
 image_path = 'images-project-1/lenna.bmp'
-image = Image.open(image_path).convert('L')  # 'L' for grayscale
+image = Image.open(image_path).convert('L')
 
 noisy_image = noise_image(image)
 
 image_fft = np.fft.fftshift(np.fft.fft2(image))
-# image_fft = np.log(np.abs()))
 noisy_image_fft = np.fft.fftshift(np.fft.fft2(noisy_image))
 
 filter_orders = [3, 5, 7]
@@ -50,6 +50,6 @@ for i, filter_order in enumerate(filter_orders):
     filtered = butterworth_low_pass_filter(image_fft, filter_order, 10)
     plt.subplot(1, len(filter_orders), i + 1)
     plt.imshow(filtered, cmap='gray')
-    plt.title(f'Filtered Image (Size {filter_order}x{filter_order})')
+    plt.title(f'Filtered Image (n={filter_order} - D0={20})')
 
 plt.show()
